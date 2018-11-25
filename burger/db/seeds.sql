5678d8a92da3e4880a19f394869897c2aa1b0b7d
T-Rex <tamhartman@gmail.com>
2018-11-24 20:10:30 -0500

    added required files and double checked them

---
+++ burger/db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Ruta-Bag-A Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Peas and Thank You Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Smells Like Bean Spirit Burger', FALSE);
